56df5fb67d4f1fa228fd187b2a69008ecdde354c
King Flame <nawhitelocke@gmail.com>
2023-12-02 18:48:48 -0500

    Completed Functionality

--- info2180-lab5-master/world.js
+++ info2180-lab5-master/world.js
@@ -1,12 +1,14 @@
 document.addEventListener( 'DOMContentLoaded', () =>{
 	
-	var srchbtn = document.getElementById("lookup");
+	var coSrchBtn = document.getElementById("lookup-country");
+    var ciSrchBtn = document.getElementById("lookup-city");
 	//var txtval = document.getElementById("country").value;
 	
-	srchbtn.addEventListener('click', function(e) {
+	coSrchBtn.addEventListener('click', function(e) {
+		e.preventDefault();
 		var txtval = document.getElementById("country").value;
 		
-		fetch("world.php?country="+encodeURI(txtval),{method: 'GET'})
+		fetch("world.php?country="+encodeURI(txtval)+"&want=country",{method: 'GET'})
             .then(response => {
                 if (response.ok) {
                     return response.text()
@@ -15,20 +17,66 @@ document.addEventListener( 'DOMContentLoaded', () =>{
                 }
             })
             .then(data => {
-                alert(data);
-				console.log(data)
+                //alert(data);
+				//console.log(data);
+				var countryArr = data.trim();
+				//console.log(countryArr);
+				SearchDisplay(countryArr);
             })
-            .catch(error => console.log('There was an error: ' + error));e.preventDefault();
+            .catch(error => console.log('There was an error: ' + error));
 		
+	});//end srchbtn on-click
+
+    ciSrchBtn.addEventListener('click', function(e) {
+		e.preventDefault();
+		var txtval = document.getElementById("country").value;
 		
-		
+		fetch("world.php?country="+encodeURI(txtval)+"&want=city",{method: 'GET'})
+            .then(response => {
+                if (response.ok) {
+                    return response.text()
+                } else {
+                    return Promise.reject('something went wrong!')
+                }
+            })
+            .then(data => {
+                //alert(data);
+				//console.log(data);
+				var countryArr = data.trim();
+				//console.log(countryArr);
+				SearchDisplay(countryArr);
+            })
+            .catch(error => console.log('There was an error: ' + error));
+
 	});//end srchbtn on-click
 	
 });//end page onload 
 
+function SearchDisplay(country){
+	
+	var res = document.getElementById('result');
+	res.innerHTML = country;
+}
+
+
 
 
 /*
+		||----------------------THUS BEGINS THE CODE GRAVEYARD----------------------||
+
+
+var i = 0;
+	while (i < length(country)){
+		var c_div = document.getElementById('result');
+		var head2 = document.createElement('h2');
+		var txt1 = document.createTextNode();
+	}//end while
+	
+
+function ProData(data){
+	return data.split("::");
+}//end process data funciton
+
 fetch("world.php",{method: 'GET', headers:{document.getElementById("country"): txtval}})
             .then(response => {
                 if (response.ok) {
--- info2180-lab5-master/world.php
+++ info2180-lab5-master/world.php
@@ -1,4 +1,5 @@
 <?php
+
 $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
@@ -9,36 +10,108 @@ $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-//echo (var_dump($_SERVER['REQUEST_METHOD']))
-
+//echo (var_dump($_SERVER['REQUEST_METHOD']));
+//echo (var_dump($_GET));
 if (isset($_GET['country'])) {
 	//get and sanitize input
 	$inp = $_GET['country'];
 	$inp = $inp.trim(" ");
-	$name = filter_var($inp, FILTER_SANITIZE_STRING);
-	$name = strip_tags($name);
+	//$name = filter_var($inp, FILTER_SANITIZE_STRING);
+	$name = strip_tags($inp);
 	//echo ("<p>".$name."</p>");}/*
 	
-	//pull from table
-	$sconn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-	$sstmt = $sconn->prepare("SELECT * FROM countries WHERE name LIKE '%$name%'");
+	if($_GET['want'] === 'country'){
+		//pull from table
+		$sstmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$name%'");
 
-	$sresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	
-	//return results
-	echo toHTML($sresults);
-	
-	
-}//remove if comment block is moved below point A
-	
+		$sresults = $sstmt->fetchAll(PDO::FETCH_ASSOC);
+		
+		//return results
+		echo allCountries($sresults);
+	}
+	elseif($_GET['want'] === 'city'){
+		$sstmt = $conn->query("SELECT * FROM cities WHERE name LIKE '%$name%'");
 
-function toHTML($arr){
+		$sresults = $sstmt->fetchAll(PDO::FETCH_ASSOC);
+
+		echo allCities($sresults);
+	}
 	
 }
 
+function allCountries($sresults){
+	$cString  = '
+        <table>
+            <tr>
+                <th>Country Name</th>
+                <th>Continent</th>
+                <th>Independence Year</th>
+                <th>Head of State</th>
+            </tr>';
+	//$string_count = 1;
+	foreach($sresults as $country){
+		
+		$cString = $cString.'
+			<tr>
+				<td>' . $country['name'] . '</td>
+				<td>' . $country['continent'] . '</td>
+				<td>' . $country['independence_year'] . '</td>
+				<td>' . $country['head_of_state'] . '</td>
+			</tr>';
+
+	}//end for each
+	$cString = $cString.'
+		</table>';
+	
+	return $cString;
+}
+
+function allCities($sresults){
+	$cString  = '
+        <table>
+            <tr>
+                <th>City Name</th>
+                <th>Country Code</th>
+                <th>District</th>
+                <th>Population</th>
+            </tr>';
+	//$string_count = 1;
+	foreach($sresults as $city){
+		
+		$cString = $cString.'
+			<tr>
+				<td>' . $city['name'] . '</td>
+				<td>' . $city['country_code'] . '</td>
+				<td>' . $city['district'] . '</td>
+				<td>' . $city['population'] . '</td>
+			</tr>';
+
+	}//end for each
+	$cString = $cString.'
+		</table>';
+	
+	return $cString;
+}
+
+?>
 
 
-	/*
+<?php
+/* 
+		||----------------------THUS BEGINS THE CODE GRAVEYARD----------------------||
+	
+	
+	function toHTML($arr){
+		return '
+		<ul>
+			<li>'.$arr['name'].'</li>SP@CE
+			<li>'.$arr['continent'].'</li>SP@CE
+			<li>'.$arr['independence_year'].'</li>SP@CE
+			<li>'.$arr['head_of_state'].'</li>
+		</ul>';
+	}
+	
+	
 	if (empty($name)) {
 		echo "";
 	} else {
@@ -63,7 +136,8 @@ function toHTML($arr){
 
 
 
-<?php foreach ($results as $row): ?>
+<ul>
+<?php foreach ($sresults as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
@@ -77,5 +151,36 @@ $_SERVER["REQUEST_METHOD"] == "GET"
 	$sstmt = $sconn->prepare("SELECT * FROM countries WHERE name = :cname");
 	$sstmt->bindParam(':cname', $name);
 	$sstmt->execute();
+	
+	
+	'
+        <table>
+            <tr>
+                <th>Country Name</th>
+                <th>Continent</th>
+                <th>Independence Year</th>
+                <th>Head of State</th>
+            </tr>
+            <tr>
+                <td>' . $data['name'] . '</td>
+                <td>' . $data['continent'] . '</td>
+                <td>' . $data['independence_year'] . '</td>
+                <td>' . $data['head_of_state'] . '</td>
+            </tr>
+        </table>
+    ';
+	
+	
+	
+			if($string_count == 1){
+			$cString= $cString.$country['name']."::".$country['continent']."::".$country['independence_year']."::".$country['head_of_state'];
+			//$string_count == $string_count + 1;
+		}//end if
+		else{
+			$cString= $cString."##||##".$country['name']."::".$country['continent']."::".$country['independence_year']."::".$country['head_of_state'];
+			//$string_count == $string_count + 1;
+		
+		}//end else if
+		
 */
 ?>
